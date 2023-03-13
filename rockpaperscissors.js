96b0a0102d0596f65f82dbb9baf8bf42b86af782
Rayden Sia <85201523+raydensia@users.noreply.github.com>
2023-03-13 21:06:49 +0800

    finish javascript code for rock-paper-scissors

---
+++ rockpaperscissors.js
@@ -0,0 +1,67 @@
+const choices = ["Rock", "Paper", "Scissors"];
+let playerSelection;
+let computerSelection;
+
+function getComputerChoice() {
+    return choices[Math.floor(Math.random()*3)];
+}
+
+function getPlayerChoice() {
+    let validInput = false;
+    while(!validInput) {
+        playerSelection = prompt("Choose your weapon!");
+        validInput = choices.includes(capitalize(playerSelection));
+    }
+    return playerSelection;
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    if (playerSelection == computerSelection) {
+        console.log("It's a draw! Try again");
+        return 0;
+    }
+    else if (
+        playerSelection == "rock" && computerSelection == "paper" ||
+        playerSelection == "paper" && computerSelection == "scissors" ||
+        playerSelection == "scissors" && computerSelection == "rock"
+    ) {
+        console.log(`You lose! ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}`);
+        return -1;
+    }
+    else {
+        console.log(`You win! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}`);
+        return 1;
+    }
+}
+
+function game(handicap = 0) {
+    let result = 0;
+    let playerWins = handicap;
+    let computerWins = 0;
+    
+    for (let i = 0; i < 5; i++) {
+        playerSelection = getPlayerChoice();
+        computerSelection = getComputerChoice();
+        result = playRound(playerSelection, computerSelection);
+        if (result == -1)
+            computerWins += 1;
+        else if (result == 1)
+            playerWins += 1;
+    }
+    if (computerWins > playerWins)
+        console.log("You have lost! Computer is the winner");
+    else if (computerWins < playerWins)
+        console.log("You have won!");
+    else
+        console.log("It is a draw")
+}
+
+function capitalize(string) {
+    string = string.toLowerCase();
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+game();
